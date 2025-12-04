4059cab798ecccf2785eeb4340d7a0b66ae10c03
jetspaser <jetspaser@mail.ru>
2025-12-04 15:42:13 +0300

    task item 4 version 1.1

--- data_types/task_1.py
+++ data_types/task_1.py
@@ -2,22 +2,22 @@
 
 # Список всех immutable (неизменяемых)
 
-# int (immutable)
+# int (immutable) - это неизменяемый (immutable) тип данных для представления целых чисел в Python.
 number: int = 10
 print(type(number))
 assert isinstance(number, int)
 
-# str (immutable)
+# str (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий текстовые данные как последовательность символов Unicode
 line: str = 'text'
 print(type(line))
 assert isinstance(line, str)
 
-# float (immutable)
+# float (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий числа с плавающей точкой (дробные числа)
 floating: float = 10.5
 print(type(floating))
 assert isinstance(floating, float)
 
-# bool (immutable)
+# bool (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий логические значения: True (истина) или False (ложь)
 true_false: bool = True
 print(type(true_false))
 assert isinstance(true_false, bool)
@@ -27,29 +27,35 @@ the_tuple: tuple = (42,)
 print(type(the_tuple))
 assert isinstance(the_tuple, tuple)
 
-# frozenset (immutable)
-n_frozenset = frozenset([1, 2, 3])
-print(type(n_frozenset))
+# frozenset (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий неупорядоченную коллекцию уникальных элементов (как set, но нельзя изменять)
+frozen: frozenset = frozenset([1, 2, 3])
+print(type(frozen))
+assert isinstance(frozen, frozenset)
 
-# bytes (immutable)
-n_bytes = b"hello"
-print(type(n_bytes))
+# bytes (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий последовательность байтов (сырые бинарные данные, числа 0-255)
+byte: bytes = b"text"
+print(type(byte))
+assert isinstance(byte, bytes)
 
 
 # Список всех mutable (изменяемых)
 
-# list (mutable)
+# list (mutable) - это изменяемый (mutable) тип данных в Python, представляющий упорядоченную коллекцию элементов с возможностью изменения
 number: list = [1, 2, 3]
 print(type(number))
+assert isinstance(byte, bytes)
 
-# dict (mutable)
+# dict (mutable) - это изменяемый (mutable) тип данных в Python, представляющий неупорядоченную коллекцию пар ключ-значение (ассоциативный массив)
 user: dict[str, int] = {"name": "Илья", "age": 26}
 print(type(user))
+assert isinstance(byte, bytes)
 
-# set (mutable)
-n_bytes = b"hello"
-print(type(n_bytes))
+# set (mutable) - это изменяемый (mutable) тип данных в Python, представляющий неупорядоченную коллекцию уникальных элементов.
+collection: set[int] = {1, 2, 3, 3, 2}
+print(type(collection))
+assert isinstance(collection, set)
 
-# bytearray (mutable)
-n_bytes = b"hello"
-print(type(n_bytes))
+# bytearray (mutable)  - это изменяемый (mutable) тип данных в Python, представляющий изменяемую последовательность байтов (аналог bytes, но можно изменять)
+byte_array: bytearray = bytearray(b"text")
+print(type(bytearray))
+assert isinstance(byte_array, bytearray)
