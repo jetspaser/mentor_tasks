acbd3d04ee2d638802e7ae85a6a9ed6758d844e6
jetspaser <jetspaser@mail.ru>
2025-12-04 15:12:15 +0300

    task item 4 version 1.0

--- data_types/task_1.py
+++ data_types/task_1.py
@@ -1,2 +1,55 @@
-a=1
-print(a)
+# Встроенные типы данных в Python
+
+# Список всех immutable (неизменяемых)
+
+# int (immutable)
+number: int = 10
+print(type(number))
+assert isinstance(number, int)
+
+# str (immutable)
+line: str = 'text'
+print(type(line))
+assert isinstance(line, str)
+
+# float (immutable)
+floating: float = 10.5
+print(type(floating))
+assert isinstance(floating, float)
+
+# bool (immutable)
+true_false: bool = True
+print(type(true_false))
+assert isinstance(true_false, bool)
+
+# tuple (immutable) - "Кортеж — это неизменяемая (immutable) упорядоченная коллекция элементов в Python. Как список, но нельзя изменить после создания."
+the_tuple: tuple = (42,)
+print(type(the_tuple))
+assert isinstance(the_tuple, tuple)
+
+# frozenset (immutable)
+n_frozenset = frozenset([1, 2, 3])
+print(type(n_frozenset))
+
+# bytes (immutable)
+n_bytes = b"hello"
+print(type(n_bytes))
+
+
+# Список всех mutable (изменяемых)
+
+# list (mutable)
+number: list = [1, 2, 3]
+print(type(number))
+
+# dict (mutable)
+user: dict[str, int] = {"name": "Илья", "age": 26}
+print(type(user))
+
+# set (mutable)
+n_bytes = b"hello"
+print(type(n_bytes))
+
+# bytearray (mutable)
+n_bytes = b"hello"
+print(type(n_bytes))
