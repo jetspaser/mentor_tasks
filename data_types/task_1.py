a449a5ec161588bd48eaa3b51eeb1b064c3f38b4
jetspaser <jetspaser@mail.ru>
2025-12-15 17:44:05 +0300

    Adding signatures for all data types

--- data_types/task_1.py
+++ data_types/task_1.py
@@ -2,37 +2,37 @@
 
 # Список всех immutable (неизменяемых)
 
-# int (immutable) - это неизменяемый (immutable) тип данных для представления целых чисел в Python.
+# int (immutable)
 number: int = 10
 print(type(number))
 assert isinstance(number, int)
 
-# str (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий текстовые данные как последовательность символов Unicode
-line: str = 'text'
+# str (immutable)
+line: str = "text"
 print(type(line))
 assert isinstance(line, str)
 
-# float (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий числа с плавающей точкой (дробные числа)
+# float (immutable)
 floating: float = 10.5
 print(type(floating))
 assert isinstance(floating, float)
 
-# bool (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий логические значения: True (истина) или False (ложь)
+# bool (immutable)
 true_false: bool = True
 print(type(true_false))
 assert isinstance(true_false, bool)
 
-# tuple (immutable) *кортеж* - это неизменяемая (immutable) упорядоченная коллекция элементов в Python. Как список, но нельзя изменить после создания.
-the_tuple: tuple[int] = (42,)
+# tuple (immutable)
+the_tuple: tuple[int, ...] = (42,)
 print(type(the_tuple))
 assert isinstance(the_tuple, tuple)
 
-# frozenset (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий неупорядоченную коллекцию уникальных элементов (как set, но нельзя изменять)
-frozen: frozenset = frozenset([1, 2, 3])
+# frozenset (immutable)
+frozen: frozenset[int] = frozenset([1, 2, 3])
 print(type(frozen))
 assert isinstance(frozen, frozenset)
 
-# bytes (immutable) - это неизменяемый (immutable) тип данных в Python, представляющий последовательность байтов (сырые бинарные данные, числа 0-255)
+# bytes (immutable)
 byte: bytes = b"text"
 print(type(byte))
 assert isinstance(byte, bytes)
@@ -40,22 +40,22 @@ assert isinstance(byte, bytes)
 
 # Список всех mutable (изменяемых)
 
-# list (mutable) - это изменяемый (mutable) тип данных в Python, представляющий упорядоченную коллекцию элементов с возможностью изменения
-number: list = [1, 2, 3]
+# list (mutable)
+number: list[int] = [1, 2, 3]
 print(type(number))
 assert isinstance(number, list)
 
-# dict (mutable) - это изменяемый (mutable) тип данных в Python, представляющий неупорядоченную коллекцию пар ключ-значение (ассоциативный массив)
+# dict (mutable)
 user: dict[str, int] = {"name": "Илья", "age": 26}
 print(type(user))
 assert isinstance(user, dict)
 
-# set (mutable) - это изменяемый (mutable) тип данных в Python, представляющий неупорядоченную коллекцию уникальных элементов.
+# set (mutable)
 collection: set[int] = {1, 2, 3, 3, 2}
 print(type(collection))
 assert isinstance(collection, set)
 
-# bytearray (mutable) - это изменяемый (mutable) тип данных в Python, представляющий изменяемую последовательность байтов (аналог bytes, но можно изменять)
+# bytearray (mutable)
 byte_array: bytearray = bytearray(b"text")
 print(type(bytearray))
 assert isinstance(byte_array, bytearray)
