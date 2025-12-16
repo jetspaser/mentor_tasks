6adccfa3f55b68f09007173ba286c19efbad3a53
jetspaser <jetspaser@mail.ru>
2025-12-16 14:42:48 +0300

    Complete task_2_unique_min_max

--- simple_def/task_2_unique_min_max.py
+++ simple_def/task_2_unique_min_max.py
@@ -2,20 +2,17 @@
 Реализовать функцию. Функция на вход принимает список list[int] произвольной длины.
 Печатает в консоль наименьшее и наибольшее УНИКАЛЬНОЕ число из начального списка.
 """
+
 def print_unique_min_max_set(values: list[int]) -> None:
     """
-    Печатает минимальное и максимальное уникальные значения списка.
+    values - произвольный список
     """
-    unique_values: set[int] = {value for value in values}
+
+    unique_values: set[int] = {value for value in values if values.count(value) == 1}
 
     if unique_values:
         print(min(unique_values), max(unique_values))
     else:
         print("Список пуст")
 
-
-if __name__ == "__main__":
-    test_data: list[int] = [3, 1, 2, 3, 5, 1]
-
-    print_unique_min_max_set(test_data)
-    print_unique_min_max_set([])
+print_unique_min_max_set([3, 1, 2, 3, 5, 1, 6, 6])
